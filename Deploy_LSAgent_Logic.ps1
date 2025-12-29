e5affe92790ea6b8aa9acc361227f905353301ff
RefinedSilica <32622711+jbakes97@users.noreply.github.com>
2025-12-29 14:30:58 -0500

    Add files via upload

---
+++ Deploy_LSAgent_Logic.ps1
@@ -0,0 +1,61 @@
+﻿#Published Date - 12/4/2025
+#Developer - Jared Bakes
+#Purpose - Controls to deploy LsAgent Version 12.2.0.1 32-Bit to all client PC's in AD
+
+$LogFile = "\\pssrv\CorpForum\Logs\Deploy_LSAgent_Logic.log"
+
+function WriteLog
+{
+Param ([string]$LogString)
+$Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+$LogMessage = "$Stamp $LogString"
+Add-content $LogFile -value $LogMessage -Encoding UTF8
+}
+
+#Declare Variable for default LsAgent Location
+$win64RootFolder = 'C:\Program Files (x86)\LansweeperAgent'
+$win32RootFolder = 'C:\Program Files\LansweeperAgent'
+#Delcare variables necessary from endpoint
+$ComputerNameString = $env:COMPUTERNAME
+#$OSVersionString = (Get-CimInstance Win32_OperatingSystem).version
+$OSVersionString = Get-WmiObject -class Win32_OperatingSystem | select Version
+$OSVersionStringReduced = $OSVersionString -replace "[^0-9.\s]",""
+
+switch -Wildcard ($OSVersionStringReduced){
+#Windows 2008R2 - 2012, 7, 8
+"6.*" {
+    #Check if the file path already exists on endpoint
+    #Calls Sysvol on domain controller to deploy exe
+    if((Test-Path -Path $win64RootFolder)){
+        WriteLog "Script 6.* did not write to $ComputerNameString with 64 bit variable due to Lansweeper already installed."
+    }
+    elseif((Test-Path -Path $win32RootFolder)){
+        WriteLog "Script 6.* did not write to $ComputerNameString with 32 bit variable due to Lansweeper already installed."
+    }
+    Else{
+        Start-Process -FilePath "\\corp.sgstool.com\SYSVOL\corp.sgstool.com\exe\LsAgent-windows_12.2.0.1.exe" -ArgumentList ' --server Lansweeper.corp.sgstool.com --port 9524 --agentkey ece3aac3-fc25-4ba2-a318-a8ef69d6966e --mode unattended'
+        WriteLog "Script 6.* launched executable on $ComputerNameString"
+    }
+}
+#Windows 10, 11
+"10.*" {
+    #Check if the file path already exists on endpoint
+    #Calls Sysvol on domain controller to deploy exe
+    if((Test-Path -Path $win64RootFolder)){
+        WriteLog "Script 10.* did not write to $ComputerNameString with 64 bit variable due to Lansweeper already installed."
+    }
+    elseif((Test-Path -Path $win32RootFolder)){
+        WriteLog "Script 10.* did not write to $ComputerNameString with 32 bit variable due to Lansweeper already installed."
+    }
+    Else{
+        Start-Process -FilePath "\\corp.sgstool.com\SYSVOL\corp.sgstool.com\exe\LsAgent-windows_12.2.0.1.exe" -ArgumentList ' --server Lansweeper.corp.sgstool.com --port 9524 --agentkey ece3aac3-fc25-4ba2-a318-a8ef69d6966e --mode unattended'
+        WriteLog "Script 10.* launched executable on $ComputerNameString"
+    }
+}
+default{
+   WriteLog "No Matches Found for $ComputerNameString skipping machine $ComputerNameString"
+    }
+}
+
+
+
